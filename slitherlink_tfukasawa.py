23fa29281fc52ac2a863237bce3d0714c83f48db
tsujitaa <programing1031@yahoo.co.jp>
2016-02-06 11:43:08 +0900

    Update slitherlink_tfukasawa.py

--- slitherlink_tfukasawa.py
+++ slitherlink_tfukasawa.py
@@ -1,5 +1,35 @@
 #スリザーリンク4*4のmaster
 N=4
 M=4
-col=[[0 for i in range(N)] for j in range(M+1)]
-print(col)
+col=[[1 for i in range(N)] for j in range(M+1)]
+row=[[1 for i in range(N+1)] for j in range(M)]
+
+def link(s,t):
+    if s<N and t<M:
+        c=col[s][t]+col[s][t-1]+row[s][t]+row[s-1][t]
+    elif s==N and t!=M:
+        c=col[s][t]+col[s][t-1]+row[s-1][t]
+    elif s!=N and t==M:
+        c=col[s][t-1]+row[s][t]+row[s-1][t]
+    else:
+        c=col[s][t-1]+row[s-1][t]
+    
+    if c==0 or c==2:
+        return True
+    else:
+        return False
+        
+def allink(s,t):
+    for i in range(s):
+        for j in range(t):
+            if link(i,j)==False:
+                return False
+
+def solved(a):
+    if a==None:
+        print(col)
+        print(row)
+    else:
+        print("unsolved")
+
+solved(allink(N+1,M+1))
